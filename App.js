d63946842b11c0ed3c7708dadda6f11770b1eda5
suhyun1 <suhyun7079@gmail.com>
2020-04-08 00:10:17 +0900

    feat: Add preloading asset and cache

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { AppLoading, Font, Asset } from "expo";
+import { Text, View, AsyncStorage } from 'react-native';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import ApolloProvider from "react-apollo-hooks";
+import apolloClientOptions from './apollo';
+
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const preLoad = async() => {
+    try{
+      await Font.loadAsync({
+        ...Ionicons.font,
+      });
+      await Asset.loadAsync(require("./assets/logo.png"));
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+
+      setLoaded(true);
+      setClient(client);
+    }catch(e){
+      console.log(e);
+    }
+    
+    setLoaded(true);
+  };
+  useEffect(()=> {
+    preLoad();
+  }, []);
+
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Hi</Text>
+      </View>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
+  );
+}
+
