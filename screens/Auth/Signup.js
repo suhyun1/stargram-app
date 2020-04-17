c290b88fd8ac55ad8dfd1e001f30290de264a250
suhyun1 <suhyun7079@gmail.com>
2020-04-17 23:51:12 +0900

    feat(auth): Add facebook Login

--- screens/Auth/Signup.js
+++ screens/Auth/Signup.js
@@ -6,6 +6,7 @@ import useInput from "../../hooks/useInput";
 import { Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { useMutation } from "react-apollo-hooks";
 import { CREATE_ACCOUNT } from "./AuthQueries";
+import * as Facebook from 'expo-facebook';
 
 const View = styled.View`
   justify-content: center;
@@ -13,10 +14,16 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const FBContainer = styled.View`
+  margin-top: 25px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-color: ${props=> props.theme.lightGreyColor};
+  border-style: solid;
+`;
+
 
 export default ({ navigation, route }) => {
-  // const emailInput = useInput("");
   const emailInput = useInput(route.params ? route.params.email : "");
   const fNameInput = useInput("");
   const lNameInput = useInput("");
@@ -68,6 +75,36 @@ export default ({ navigation, route }) => {
       setLoading(false);
     }
   };
+
+  const fbLogin = async() => {
+    try{
+      setLoading(true);
+      await Facebook.initializeAsync("242065393832323");
+      const {
+        type,
+        token
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile', "email"],
+      });
+      console.log(type);
+      console.log(token);
+      if(type === "success"){
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`);
+        const {email, first_name, last_name} = await response.json();
+        emailInput.setValue(email);
+        fNameInput.setValue(first_name);
+        lNameInput.setValue(last_name);
+        const [username] = email.split("@");
+        usernameInput.setValue(username); //email 주소 @ 앞부분을 username으로 가져옴
+        setLoading(true);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+
+  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View>
@@ -95,6 +132,14 @@ export default ({ navigation, route }) => {
           autoCorrect={false}
         />
         <AuthButton loading={loading} text={"Sign up"} onPress={handleSignup} />
+        <FBContainer>
+          <AuthButton
+            bgColor={"#2D4DA7"}
+            loading={false}
+            onPress={fbLogin}
+            text="Connect Facebook"
+          />
+        </FBContainer>
       </View>
     </TouchableWithoutFeedback>
   );
