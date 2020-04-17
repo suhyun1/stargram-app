32d93c4c13bbeab169a7bd9f763b6553b191cea8
suhyun1 <suhyun7079@gmail.com>
2020-04-17 23:49:13 +0900

    fix: Add background color option

--- components/AuthButton.js
+++ components/AuthButton.js
@@ -7,7 +7,7 @@ import { ActivityIndicator } from "react-native";
 
 const Touchable = styled.TouchableOpacity``;
 const Container = styled.View`
-  background-color: ${props => props.theme.blueColor};
+  background-color: ${props => props.bgColor ? props.bgColor : props.theme.blueColor};
   padding: 10px;
   margin: 0px 50px;
   border-radius: 4px;
@@ -19,9 +19,9 @@ const Text = styled.Text`
   font-weight: 600;
 `;
 
-const AuthButton = ({text, onPress, loading=false}) => (
+const AuthButton = ({text, onPress, loading=false, bgColor=null}) => (
     <Touchable disable={loading} onPress={onPress}>
-        <Container>
+        <Container bgColor={bgColor}>
           {loading ? <ActivityIndicator color={"white"}/> : <Text>{text}</Text>}
            
         </Container>
